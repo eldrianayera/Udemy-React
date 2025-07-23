76b1bc26f84824d96c02e956e94b6786351d5ad1
Eldrian <eldrianayera710@gmail.com>
2025-07-23 20:12:39 +0800

    login-page : add isRegistered rendering

--- login-page/src/components/App.jsx
+++ login-page/src/components/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Login from "./Login"
 
-var isLoggedIn = true;
-
+var isLoggedIn = false;
 
 
 
--- login-page/src/components/Login.jsx
+++ login-page/src/components/Login.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Input from "./Input";
 
+var isRegistered = true;
+
+
 function Login() {
     return (<form className="form">
         <Input type="text" placeholder="Username" />
         <Input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        {isRegistered === false && <Input type="password" placeholder="Confirm Password" /> }
+        {isRegistered? <button type="submit">Login</button> : <button type="submit">Register</button> }
       </form>);
 }
 
