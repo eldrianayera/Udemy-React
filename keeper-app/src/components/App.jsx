ebba6a7c2f9ff86cfbcf818aa2de7d14da6e4707
Eldrian <eldrianayera710@gmail.com>
2025-07-23 14:14:16 +0800

    keeper-app : add App.jsx

---
+++ keeper-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Keeper App</h1>
+      {/* Other components like Header, Footer, Note can be added here */}
+    </div>
+  );
+}
+
+export default App;
--- keeper-app/src/index.jsx
+++ keeper-app/src/index.jsx
@@ -11,8 +11,9 @@
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom"
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App /> , document.getElementById("root"));
+
