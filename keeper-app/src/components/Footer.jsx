0e6f71bce7efd3c4d53c09bc2ed46b54cddc74e0
Eldrian <eldrianayera710@gmail.com>
2025-07-23 14:25:27 +0800

    keeper-app : add Note.jsx

--- keeper-app/src/components/Footer.jsx
+++ keeper-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <p>Copyright © {new Date().getFullYear()}</p>
+            <p>Copyright © {new Date().getFullYear()} Eldrian</p>
         </footer>
     );
 }
