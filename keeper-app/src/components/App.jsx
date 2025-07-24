4a021f45356b5f0eecbe19f58cbd3405e0c6ee2d
Eldrian <eldrianayera710@gmail.com>
2025-07-24 17:29:18 +0800

    keeper-app : complete add mechanism

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import Form from "./Form";
-import notes from "../notes"
-
+import notes from "../notes";
 
 function App() {
+  const [contentArray, setContentArray] = useState([]);
+
+  function addItem(inputText) {
+    setContentArray((prevArray) => {
+      return [...prevArray, inputText];
+    });
+    console.log(contentArray);
+  }
+
   return (
     <div>
       <Header />
-      <Form />
-      {notes.map(notes => (
-        <Note 
-        key={notes.key}
-        title={notes.title} 
-        content={notes.content}  />
-        ))}
+      <Form onAdd={addItem} />
+      {contentArray.map((item) => (
+        <Note key={item.key} title={item.title} content={item.content} />
+      ))}
       <Footer />
     </div>
   );
 }
 
-
-export default App; 
+export default App;
--- keeper-app/src/components/Form.jsx
+++ keeper-app/src/components/Form.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function form() {
+// ADD KEY FOR EACH CHILD
+// BUILD DELETE FUNCTION AND BUTTON
+
+function form(props) {
+  const [inputText, setInputText] = useState({
+    title: "",
+    content: "",
+  });
+
+  function updateInputText(event) {
+    const { name, value } = event.target;
+
+    setInputText((prevArray) => {
+      return {
+        ...prevArray,
+        [name]: value,
+      };
+    });
+  }
+
   return (
-    <div className="note">
-      <input type="text" />
-      <input type="text" />
-    </div>
+    <form>
+      <input
+        onChange={updateInputText}
+        name="title"
+        placeholder="Title"
+        type="text"
+        value={inputText.title}
+      />
+      <textarea
+        onChange={updateInputText}
+        name="content"
+        placeholder="Take a note..."
+        rows="3"
+        value={inputText.content}
+      />
+      <button
+        onClick={() => {
+          event.preventDefault();
+          props.onAdd(inputText);
+          setInputText({
+            title: "",
+            content: "",
+          });
+        }}
+      >
+        add
+      </button>
+    </form>
   );
 }
 
